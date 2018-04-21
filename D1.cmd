219d6ffcfa59ded85342255e45ebecffb9307083
JoshuaMathias <emathias@patas.ling.washington.edu>
2018-04-21 16:39:11 -0700

    Running main

---
+++ D1.cmd
@@ -0,0 +1,12 @@
+#-------------------------------------
+# Run summarizer.sh
+#-------------------------------------
+universe        = vanilla
+executable      = bin/summarizer
+getenv          = true
+output          = sum.out
+error           = sum.err
+log             = sum.log
+arguments       = ""
+transfer_executable = false
+queue
