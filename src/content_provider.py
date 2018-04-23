0d806328cd1bb13048cb71dfd2487978d47f0908
John Greve <jgreve@u.washington.edu>
2018-04-22 22:20:04 -0700

    debugging for first run on patas

--- src/content_provider.py
+++ src/content_provider.py
@@ -1,6 +1,10 @@
 import bs4
 import argparse
 
+import local_util as u
+# "local_util" mainly for u.eprint() which is like print but goes to stderr.
+
+
 class ArticleContent():
     def __init__(self, id='', type = 'story', headline='', dateline='', date='', body=list()):
         self.id = id
@@ -9,6 +13,9 @@ class ArticleContent():
         self.dateline = dateline
         self.date = date
         self.setBody(body)
+
+    def __str__(self):
+        return 'ArticleContent(): headline="{}"'.format(self.headline)
 
     def setBody(self, bodyText):
         self.body = bodyText
@@ -28,8 +35,10 @@ class DocumentSet():
         self.topic_id = topic_id
         self.topic_cat = topic_cat
         self.topic_title = topic_title
-
         self.docs = list()
+
+    def __str__(self):
+        return 'DocumentSet(): topic_id="{}" #docs={}'.format( self.topic_id, len(self.docs) )
 
     def addDoc(self, doc):
         self.docs.append(doc)
@@ -40,6 +49,9 @@ class ContentReader():
                  aquaint2 = '/opt/dropbox/17-18/573/AQUAINT-2'):
         self.AQUAINT_DIR = aquaint
         self.AQUAINT2_DIR = aquaint2
+
+    def __str__(self):
+        return 'ContentReader()'
 
     def read_raw_files(self, *filenames):
         articles = list()
@@ -93,19 +105,26 @@ class ContentReader():
         return articles
 
     def __aquaint_file__(self, doc_id):
+        u.eprint('> ContentReader.__aquaint_file__() doc_id="{}"'.format(doc_id) )
+        reason = "None"
         if doc_id[3:8] == '_ENG_':  # True for AQUAINT-2 files
+            reason = "doc_id[3:8] == '_ENG_':  # True for AQUAINT-2 files"
             filename = '%s/data/%s/%s.xml' % (self.AQUAINT2_DIR,
                                               doc_id[0:7].lower(),
                                               doc_id[0:doc_id.find('.')-2].lower())
         else:
+            reason="Not doc_id[3:8] == '_ENG_'"
             file_extension = doc_id[0:3].upper()
             if file_extension != 'NYT':
+                reason += " (also file_extension='{}' != 'NYT')".format(file_extension)
                 file_extension += '_ENG'
             filename = '%s/%s/%s/%s_%s' % (self.AQUAINT_DIR,
                                          doc_id[0:3].lower(),
                                          doc_id[3:7],
                                          doc_id[3:doc_id.find('.')],
                                          file_extension)
+
+        u.eprint('|   filename="{}" w/reason={}'.format(filename, reason))
 
         art = None
 
@@ -128,19 +147,38 @@ class ContentReader():
         return art
 
     def __read_aquaint_doc__(self, doc_id):
-        return self.__aquaint_file__(doc_id)
+        u.eprint('> ContentReader.__read_aquaint_doc__(): doc_id={}, calling self.__aquaint_file__(doc_id)'.format(doc_id) )
+        x = self.__aquaint_file__(doc_id)
+        u.eprint('< ContentReader.__read_aquaint_doc__(): returning {}'.format(x))
+        return x
 
     def read_topic_index(self, filename):
+
+
+        u.eprint('ContentReader.read_topic_index(): filename={}'.format(filename))
         topicIndex = bs4.BeautifulSoup(open(filename).read(), 'html.parser')
         for topic in topicIndex.find_all('topic'):
             topic_id = topic['id']
+            u.eprint('ContentReader.read_topic_index(): topic_id={}'.format(topic_id))
             topic_cat = topic['category']
+            u.eprint('ContentReader.read_topic_index(): topic_cat={}'.format(topic_cat))
             topic_title = topic.find('title').contents[0]
+            u.eprint('ContentReader.read_topic_index(): topic_title={}'.format(topic_title))
+            docset_cnt = 0
             for docset_tag in topic.find_all('docseta'):
+                docset_cnt += 1
+                u.eprint('ContentReader.read_topic_index():     docset_cnt={}, docset_tag={}'.format( docset_cnt, docset_tag ) )
                 docset_id = docset_tag['id']
                 docset = DocumentSet(docset_id, topic_id, topic_cat, topic_title)
+                doc_cnt = 0
                 for doc in docset_tag.find_all('doc'):
-                    docset.addDoc(self.__read_aquaint_doc__(doc['id'].strip()))
+                    doc_cnt += 1
+                    # original:     docset.addDoc( self.__read_aquaint_doc__( doc['id'].strip() ) )
+                    u.eprint('\t\tdoc_cnt={}, doc={}'.format(doc_cnt, doc))
+                    thing = doc['id'].strip()
+                    u.eprint('\t\tdoc_cnt={}, calling __read_aquaint_doc__() on thing="{}"'.format( doc_cnt, thing ) )
+                    docset.addDoc(self.__read_aquaint_doc__( thing ) )
+                u.eprint('ContentReader.read_topic_index(): before yield: yielding docset={}'.format(docset))
 
                 yield docset
 
