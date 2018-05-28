de5012416db75f87084425b0bbb2cc25aec0a005
JoshuaMathias <joshuaamathias@gmail.com>
2018-05-28 14:29:18 -0700

    Consolidate to one dict, write to str

--- src/train_counts.py
+++ src/train_counts.py
@@ -9,7 +9,6 @@ import preprocess
 def train_counts(docsets, out_file):
 	word_counts = {}
 	# doc_counts = {}
-	docset_counts = {}
 	num_docsets = 0
 	preprocessor = preprocess.preprocess()
 	for docset in docsets:
@@ -17,23 +16,29 @@ def train_counts(docsets, out_file):
 		docset_words = preprocessor.words_from_docset(docset)
 		for word, count in docset_words.items():
 			if word in docset_counts:
-				docset_counts[word] += 1
-				word_counts[word] += count
+				word_stats = word_counts[word]
+				word_stats[0] += 1 # docset count
+				word_stats[1] += count # word count
 
 			else:
-				docset_counts[word] = 1
-				word_counts[word] = count
+				word_stats = []
+				word_stats.append(1)
+				word_stats.append(count)
+				word_counts[word] = word_stats
 		break # Test on only one docset
 	with open(out_file, 'w') as counts_file:
 		for word in docset_counts:
-			counts_file.write(word+"\t"+docset_counts[word]+"\t"+word_counts[word]+"\n")
+			counts_file.write(str(word)+"\t"+str(docset_counts[word])+"\t"+str(word_counts[word])+"\n")
 		counts_file.write(num_docsets+" total docsets"+"\n")
 
 # Read and return word_counts, docset_counts, num_docsets
 def read_train_counts(word_counts_file):
 	word_counts = {}
-	docset_counts = {}
 	num_docsets = 0
-	# with open(out_file, 'w') as counts_file:
-
+	with open(out_file, 'w') as counts_file:
+		for line in counts_file:
+			line = line.strip()
+			split_line = line.split()
+			if not split_line == "docsets":
+				word_counts[split_line[0]] = (split_line[1], split_line[2])
 	return word_counts, docset_counts, num_docsets
