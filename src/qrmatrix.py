8a0d28bbffeb4abe2a4a00e337d510d1d6795a34
JoshuaMathias <joshuaamathias@gmail.com>
2018-05-28 16:45:12 -0700

    Handle 0 count

--- src/qrmatrix.py
+++ src/qrmatrix.py
@@ -185,8 +185,13 @@ def qr_sum(docset, config, trained_word_counts, num_trained_docsets):
             if word in words_dict:
                 # word_val = words_tally[word]
                 word_val = get_tfdf(words_tally[word], article_count, len(words_docs[word]), lowest_df)
-                word_stats = trained_word_counts[word]
-                word_val = get_tfidf(word_val, num_trained_docsets, word_stats[1])
+                trained_word_count = 0
+                trained_docset_count = 0
+                if word in trained_word_counts:
+                    word_stats = trained_word_counts[word]
+                    trained_word_count = word_stats[0]
+                    trained_docset_count = word_stats[1]
+                word_val = get_tfidf(word_val, num_trained_docsets, trained_docset_count)
                 # word_val = get_doc_freq(article_count, len(words_docs[word]))
                 # word_val *= words_tally[word]
                 feat_vec[words_dict[word]] = word_val
