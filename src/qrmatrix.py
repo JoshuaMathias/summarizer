23a8397fa50782ac173e0eb219c3ef15fb62f954
JoshuaMathias <joshuaamathias@gmail.com>
2018-05-28 20:15:30 -0700

    True tf-idf

--- src/qrmatrix.py
+++ src/qrmatrix.py
@@ -190,8 +190,8 @@ def qr_sum(docset, config, trained_word_counts, num_trained_docsets):
                         word_stats = trained_word_counts[word]
                         trained_word_count = word_stats[0]
                         trained_docset_count = word_stats[1]
-                word_val = get_tfdf(words_tally[word] / trained_word_count, article_count, len(words_docs[word]), lowest_df)
-                # word_val = get_tfidf(word_val, num_trained_docsets, trained_docset_count)
+                # word_val = get_tfdf(words_tally[word] / trained_word_count, article_count, len(words_docs[word]), lowest_df)
+                word_val = get_tfidf(words_tally[word], num_trained_docsets, trained_docset_count)
                 # word_val = get_doc_freq(article_count, len(words_docs[word]))
                 # word_val *= words_tally[word]
                 feat_vec[words_dict[word]] = word_val
