2bdbe7480b55ee4e6a886a013889684dea907554
JoshuaMathias <joshuaamathias@gmail.com>
2018-05-28 20:30:05 -0700

    Syntax error

--- src/qrmatrix.py
+++ src/qrmatrix.py
@@ -175,7 +175,7 @@ def qr_sum(docset, config, trained_word_counts, num_trained_docsets):
 
 # CREATE FEATURE VECTORS
     highest_word_count = 0
-    for _, trained_word_stats in trained_word_counts.iter():
+    for _, trained_word_stats in trained_word_counts.items():
         if highest_word_count < trained_word_stats[0]:
             highest_word_count = trained_word_stats[0]
     lowest_df = math.log(lowest_word_docs / highest_word_count) / (1 + (article_count / num_trained_docsets)) # Used to normalize (and make positive) the value of tfdf for each word
