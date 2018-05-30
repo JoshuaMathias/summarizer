27f39a74d4817b1ceeff7f37d9fe110f26caf022
Eric Lindberg <lindbe2@uw.edu>
2018-05-29 22:53:26 -0700

    Fixing summary sentences to avoid repetitions and fail over to the next in line

--- src/position_weight_summarizer.py
+++ src/position_weight_summarizer.py
@@ -30,18 +30,29 @@ class PositionWeights():
         with open(sentence_weight_file, 'r') as swf:
             self.sentence_weights = WeightMatrix(swf)
 
-        self.combined_weights = numpy.zeros((10, self.article_weights.max_horizontal, self.sentence_weights.max_horizontal))
+        self.combined_weights = numpy.zeros((10, self.article_weights.max_horizontal * self.sentence_weights.max_horizontal))
         for summary_idx in range(10):
             for article_idx in range(self.article_weights.max_horizontal):
                 for sentence_idx in range(self.sentence_weights.max_horizontal):
-                    self.combined_weights[summary_idx, article_idx, sentence_idx] = self.combined_weight(summary_idx, article_idx, sentence_idx)
+                    self.combined_weights[summary_idx, (article_idx * sentence_idx) + sentence_idx] = self.combined_weight(summary_idx, article_idx, sentence_idx)
 
     def combined_weight(self, summary_index, article_index, sentence_index):
         return self.article_weights.weight_value(summary_index, article_index) * self.sentence_weights.weight_value(summary_index, sentence_index)
 
-    def argmax(self, summary_index):
-        idx = numpy.unravel_index(numpy.argmax(self.combined_weights[summary_index], axis=None), self.combined_weights[summary_index].shape)
-        return idx
+    def sortedValues(self, summary_index):
+        return self.combined_weights[summary_index].argsort()[::-1]
+
+    def select_sentence(self, summary_idx, summary, article_list, available_space):
+        cand_idcs = self.sortedValues(summary_idx)
+        for idx in cand_idcs:
+            article_idx = idx / self.sentence_weights.max_horizontal
+            sentence_idx = idx % self.sentence_weights.max_horizontal
+            if len(article_list) > article_idx and len(article_list[article_idx]) > sentence_idx:
+                candidate_sentence = article_list[article_idx, sentence_idx]
+                if candidate_sentence not in summary and len(candidate_sentence.split()) <= available_space:
+                    return candidate_sentence
+
+        return ''
 
     def position_sum(self, docset):
         article_list = list()
@@ -54,8 +65,9 @@ class PositionWeights():
         summary = list()
         summary_text = ''
         while len(summary_text.split()) < 100:
-            candidate_sentence = article_list(self.argmax(len(summary)))
-            if len(candidate_sentence.split()) + len(summary_text.split()) < 100:
+            available_space = 100 - len(summary_text.split())
+            candidate_sentence = self.select_sentence(len(summary), summary, article_list, available_space)
+            if len(candidate_sentence) > 0:
                 if len(summary_text) > 0:
                     summary_text += ' ' + candidate_sentence
                 summary.append(candidate_sentence)
@@ -64,9 +76,10 @@ class PositionWeights():
 
         return summary_text
 
-
-
 if __name__ == "__main__":
     testWeights = PositionWeights('../outputs/position_data/sample_article_data.csv', '../outputs/position_data/sample_sentence_data.csv')
     print('sample position 0, 0, 0 is %f' % testWeights.combined_weight(0, 0, 0))
-    print('summary 0 article/sentence argmax is %d / %d' % testWeights.argmax(0))
+    print('summary 0 article/sentence sorted order is %s' % testWeights.sortedValues(0))
+    for sum_idx in range(10):
+        topvalue = testWeights.sortedValues(sum_idx)[0]
+        print('Top item for line %d corresponds to %d %d' % (sum_idx + 1, topvalue / testWeights.sentence_weights.max_horizontal, topvalue % testWeights.sentence_weights.max_horizontal))
